71c5aa117bb3fc27624ea3664023a8017b486116
hidngemini <hidngemini@gmail.com>
2025-05-13 12:00:27 -0700

    [2D] redid printing and freed memory as we go

--- 2D/pinn2D.py
+++ 2D/pinn2D.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+import time
 
 from utils2D import NeuralNetwork
 import utils2D
@@ -10,13 +11,7 @@ device = torch.accelerator.current_accelerator().type if torch.accelerator.is_av
 # which device are we using?
 print(f"Using {device} device")
     
-def train_loop(model, optimizer, num_gen, num_ic, num_bc):
-    torch.mps.empty_cache()
-    # Diclosure: I used chatGPT (as per Branson's suggestion) 
-    # to learn about inputing multiple inputs at the same time
-    # rather than training this one at a time. This is so so so
-    # much faster than what I was doing before (training
-    # one input at a time, ~10000 inputs per epoch)
+def train_loop(model, optimizer, num_gen, num_ic, num_bc, print_info):
     model.train()
 
     # general case (pde loss)
@@ -26,6 +21,11 @@ def train_loop(model, optimizer, num_gen, num_ic, num_bc):
     in_gen = torch.cat([x_gen, y_gen, t_gen], dim=1).to(device)
     loss_pde = utils2D.pdeLoss(in_gen, model, 1.0)
 
+    del x_gen
+    del y_gen
+    del t_gen
+    del in_gen
+
     # initial condition
     x_ic = torch.rand((num_ic, 1))
     y_ic = torch.rand((num_ic, 1))
@@ -34,6 +34,13 @@ def train_loop(model, optimizer, num_gen, num_ic, num_bc):
     u_ic_expected = (torch.sin(torch.pi * x_ic) + torch.sin(torch.pi * y_ic)).to(device)
     u_ic_actual = model(inputs_ic)
     loss_ic = nn.MSELoss()(u_ic_expected, u_ic_actual)
+
+    del x_ic
+    del y_ic
+    del t_ic
+    del inputs_ic
+    del u_ic_expected
+    del u_ic_actual
 
     # boundry condition on the x axis (u(0,t) = u(1,t)) 
     t_bc = torch.linspace(0, 1, num_bc).view(-1, 1).to(device)
@@ -46,6 +53,15 @@ def train_loop(model, optimizer, num_gen, num_ic, num_bc):
     out_bc_ones_x = model(inputs_ic_ones_x) # calculte u(1,y,t)s
     loss_bc_x = nn.MSELoss()(out_bc_zeros_x, torch.zeros_like(out_bc_zeros_x)) + nn.MSELoss()(out_bc_ones_x, torch.zeros_like(out_bc_ones_x))
 
+    del t_bc
+    del x_bc_zeros
+    del y_bc
+    del x_bc_ones
+    del inputs_ic_zeros_x
+    del out_bc_zeros_x
+    del inputs_ic_ones_x
+    del out_bc_ones_x
+
     # boundry condition on the x axis (u(0,t) = u(1,t)) 
     t_bc = torch.linspace(0, 1, num_bc).view(-1, 1).to(device)
     y_bc_zeros = torch.zeros_like(t_bc).to(device)
@@ -57,10 +73,27 @@ def train_loop(model, optimizer, num_gen, num_ic, num_bc):
     out_bc_ones_y = model(inputs_ic_ones_y) # calculate u(x,1,t)s
     loss_bc_y = nn.MSELoss()(out_bc_zeros_y, torch.zeros_like(out_bc_zeros_y)) + nn.MSELoss()(out_bc_ones_y, torch.zeros_like(out_bc_ones_y))
 
+    del t_bc
+    del y_bc_zeros
+    del x_bc
+    del y_bc_ones
+    del inputs_ic_zeros_y
+    del out_bc_zeros_y
+    del inputs_ic_ones_y
+    del out_bc_ones_y
+
     # Total loss
     loss = loss_pde + loss_ic + loss_bc_x + loss_bc_y
     loss.backward()
     optimizer.step()
+
+    if ((print_info.get('epoch')+1) % print_info.get('print_frequency') == 0):
+        elapsed = time.time() - print_info.get('start_time')
+        mins = int(elapsed // 60)
+        secs = int(elapsed % 60)
+        print(
+            f"Epoch {print_info.get('epoch')+1} / {print_info.get('min_epochs')} : Time = {mins}m {secs:2.0f}s Loss = {loss:.8f}"
+        )
 
     return loss
     
@@ -77,14 +110,21 @@ losses = []
 natural_minimum = float("inf")
 
 i = 0
+start_time = time.time()
 while (i<min_epochs) or (losses[-1] > natural_minimum):
-    if ((i+1) % 25 == 0):
-        print(f"Epoch {i+1}/{min_epochs}\n-------------------------------")
-        losses.append(train_loop(model, optimizer, 10000, 5000, 5000))
-        print(f"Loss: {losses[-1]}")
-        print()
-    else:
-        losses.append(train_loop(model, optimizer, 10000, 5000, 5000))
+    losses.append(train_loop(
+        model=model, 
+        optimizer=optimizer, 
+        num_gen=10000, 
+        num_ic=5000, 
+        num_bc=5000,
+        print_info = {
+            'epoch' : i,
+            'print_frequency' : 100,
+            'min_epochs' : min_epochs,
+            'start_time' : start_time
+        })
+    )
     i += 1
     if (i == min_epochs):
         natural_minimum = min(losses)
